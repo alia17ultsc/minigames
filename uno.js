8730dff8adfb00e52aedb054c2297c0db5c328b9
alia17ultsc <aliafayez2009@gmail.com>
2024-09-25 02:29:16 +0300

    clearing page when pressing on a button

--- uno.js
+++ uno.js
@@ -2,4 +2,8 @@ function hideAll(){
   //var title=document.getElementById("title");
   var title=document.getElementsByClassName("title")[0];
   title.style.display="none";
+  var entername=document.getElementById("entername");
+  entername.style.display="none";
+  var playersid=document.getElementById("playersid");
+  playersid.style.display="none";
 }
