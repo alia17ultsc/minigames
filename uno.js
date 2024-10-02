7d924d1b42201beeb55844e5a6dd2345cf217b01
alia17ultsc <aliafayez2009@gmail.com>
2024-10-02 17:43:09 +0300

    adding animation to cards uno

--- uno.js
+++ uno.js
@@ -7,3 +7,30 @@ function hideAll(){
   var playersid=document.getElementById("playersid");
   playersid.style.display="none";
 }
+
+function timeoutAnimationLoadPage(){
+  var allCards=document.getElementsByClassName("deck");
+
+  for (var cardIndex=0; cardIndex<allCards.length; cardIndex++){
+    var classname=`move${cardIndex+1}`;
+      allCards[cardIndex].classList.add(classname);
+  }
+
+  setTimeout(AnimationLoadPage, 500);
+}
+
+function AnimationLoadPage(){
+  var allCards=document.getElementsByClassName("deck");
+
+  for (var cardIndex=1; cardIndex<allCards.length; cardIndex++){
+    console.log(cardIndex);
+    var classname=`mr${cardIndex}`;
+    console.log(classname);
+    allCards[cardIndex].classList.remove(`move${cardIndex+1}`);
+      allCards[cardIndex].classList.add(classname);
+  }
+
+
+}
+
+timeoutAnimationLoadPage();
