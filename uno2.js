2a791c8f77c1c73159257288fd87a104f73937b6
alia17ultsc <aliafayez2009@gmail.com>
2024-10-12 18:10:25 +0300

    adding more js

---
+++ uno2.js
@@ -0,0 +1,20 @@
+class unoCard{
+  constructor(card_symbol){
+    this.card_symbol = card_symbol;
+    this.nextCards=[];
+    this.color="";
+  
+    }
+
+     move() {
+      
+    }
+}
+
+
+
+function clickcard(card){
+
+}
+
+gjghgjhghjg
